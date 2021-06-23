c686adc363896318ba94f47f3a0b0886a9b50494
AryanSisodiya <aryansisodiya091107@gmail.com>
2021-06-23 19:07:31 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./MyComponents/Header";
+
+function App() {
+  let myVar = "Aryan";
+  return (
+    <>
+      <Header/>
+    </>
+  );
+}
+
+export default App;
